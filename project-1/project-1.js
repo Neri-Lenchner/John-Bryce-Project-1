2c98fffee94e59bec2017df6029980bad77533fc
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2025-10-28 22:59:41 +0200

    renderNote function

--- project-1/project-1.js
+++ project-1/project-1.js
@@ -1,1 +1,20 @@
 console.log('Project 1');
+
+function renderNote() {
+  const note = document.createElement('div');
+  note.className = 'note';
+  const noteButton = document.createElement('button');
+  noteButton.className = 'note-button';
+  const noteInfo = document.createElement('div');
+  noteInfo.className = 'note-info';
+  const textErea = document.createElement('textarea');
+  textErea.className = 'text-erea';
+  const dateTimeContainer = document.createElement('div');
+  dateTimeContainer.className = 'date-time-container';
+  const date = document.createElement('div');
+  date.className = 'date';
+  const time = document.createElement('div');
+  time.className = 'time';
+  dateTimeContainer.append(date, time);
+  note.append(noteButton,noteInfo, textErea, dateTimeContainer);
+}
