8d6bcf03571c0f59f981a90fd1e910335c96d210
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2025-10-29 17:40:16 +0200

    bla bla

--- project-1/project-1.js
+++ project-1/project-1.js
@@ -21,54 +21,56 @@ function addTodo(event) {
   const time = form.todoTime.value;
 
   todoListTotal.push(new Todo(data, date, time));
-    console.log(todoListTotal);
+  renderNotes();
+  console.log(todoListTotal);
 }
 
-function resetForm(event) {
-  event.target.reset();
+function resetForm() {
+  const inputs = document.querySelectorAll('input');
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].value = '';
+  }
 }
 
-const notesMonitorElement = document.querySelector('#notes-monitor-element');
 
-function renderNote() {
-  const note = document.createElement('div');
-  note.className = 'note';
-  const noteButton = document.createElement('button');
-  noteButton.className = 'note-button'; // 'glyphicon glyphicon-remov bootstrap-button'; 
-  noteButton.innerHTML = '&times;';
-  /*
-    noteButton.innerHTML = `<!-- Custom black square + white X button -->
-    <button type="button" class="close-btn" aria-label="Close"></button>`;
-  */
-  const noteInfo = document.createElement('div');
-  noteInfo.className = 'note-info';
-  const textErea = document.createElement('textarea');
-  textErea.textContent = 'This note is givven to you by The Lench. The best Lenchner there is. If you want to check this out you can!!! ;yeah yeah yeah';
-  textErea.className = 'text-erea';
-  const dateTimeContainer = document.createElement('div');
-  dateTimeContainer.className = 'date-time-container';
-  const date = document.createElement('div');
-  date.textContent = '28/10/2025';
-  date.className = 'date';
-  const time = document.createElement('div');
-  time.className = 'time';
-  time.textContent = '13:00';
-  dateTimeContainer.append(date, time);
 
-  noteInfo.append(textErea, dateTimeContainer);
+function renderNotes() {
+  const notesMonitorElement = document.querySelector('#notes-monitor-element');
 
-  note.append(noteButton,noteInfo);
+  notesMonitorElement.innerHTML = '';
+  
+  for (let i = 0; i < todoListTotal.length; i++) {
+    const todo = todoListTotal[i];
 
-  notesMonitorElement.appendChild(note);
+    const note = document.createElement('div');
+    note.className = 'note';
+    const noteButton = document.createElement('button');
+    noteButton.className = 'note-button'; // 'glyphicon glyphicon-remov bootstrap-button'; 
+    noteButton.innerHTML = '&times;';
+    /*
+      noteButton.innerHTML = `<!-- Custom black square + white X button -->
+      <button type="button" class="close-btn" aria-label="Close"></button>`;
+    */
+    const noteInfo = document.createElement('div');
+    noteInfo.className = 'note-info';
+    const textErea = document.createElement('textarea');
+    textErea.textContent = todo.todoData;
+    textErea.className = 'text-erea';
+    const dateTimeContainer = document.createElement('div');
+    dateTimeContainer.className = 'date-time-container';
+    const date = document.createElement('div');
+    date.textContent = todo.todoDate;
+    date.className = 'date';
+    const time = document.createElement('div');
+    time.className = 'time';
+    time.textContent = todo.todoTime;
+    dateTimeContainer.append(date, time);
+
+    noteInfo.append(textErea, dateTimeContainer);
+
+    note.append(noteButton,noteInfo);
+
+    notesMonitorElement.appendChild(note);
+  }
 }
 
-renderNote();
-renderNote();
-renderNote();
-renderNote();
-renderNote();
-renderNote();
-renderNote();
-renderNote();
-renderNote();
-renderNote();
