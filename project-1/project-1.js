1320b3124400b313f1932ac07f702378ebc72bcb
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2025-10-29 18:32:18 +0200

    functionality in motion

--- project-1/project-1.js
+++ project-1/project-1.js
@@ -9,8 +9,9 @@ class Todo {
     this.todoTime = todoTime;
   }
 }
-
-const todoListTotal = [];
+const list = returnList();
+const todoListTotal = [...list];
+renderNotes1();
 
 function addTodo(event) {
   event.preventDefault();
@@ -38,19 +39,58 @@ function renderNotes() {
   const notesMonitorElement = document.querySelector('#notes-monitor-element');
 
   notesMonitorElement.innerHTML = '';
-  
+  let lastNoteElement = null;
   for (let i = 0; i < todoListTotal.length; i++) {
+
     const todo = todoListTotal[i];
 
     const note = document.createElement('div');
     note.className = 'note';
     const noteButton = document.createElement('button');
-    noteButton.className = 'note-button'; // 'glyphicon glyphicon-remov bootstrap-button'; 
+    noteButton.className = 'note-button'; 
     noteButton.innerHTML = '&times;';
-    /*
-      noteButton.innerHTML = `<!-- Custom black square + white X button -->
-      <button type="button" class="close-btn" aria-label="Close"></button>`;
-    */
+    noteButton.setAttribute('onclick', `deleteNote(${i})`);
+    const noteInfo = document.createElement('div');
+    noteInfo.className = 'note-info';
+    const textErea = document.createElement('textarea');
+    textErea.textContent = todo.todoData;
+    textErea.className = 'text-erea';
+    const dateTimeContainer = document.createElement('div');
+    dateTimeContainer.className = 'date-time-container';
+    const date = document.createElement('div');
+    date.textContent = todo.todoDate;
+    date.className = 'date';
+    const time = document.createElement('div');
+    time.className = 'time';
+    time.textContent = todo.todoTime;
+    dateTimeContainer.append(date, time);
+
+    noteInfo.append(textErea, dateTimeContainer);
+
+    note.append(noteButton,noteInfo);
+
+    notesMonitorElement.appendChild(note);
+    lastNoteElement = note;
+  }
+  if (lastNoteElement) {
+    lastNoteElement.classList.add('last-note');
+  }
+}
+
+function renderNotes1() {
+  const notesMonitorElement = document.querySelector('#notes-monitor-element');
+
+  notesMonitorElement.innerHTML = '';
+  for (let i = 0; i < todoListTotal.length; i++) {
+
+    const todo = todoListTotal[i];
+
+    const note = document.createElement('div');
+    note.className = 'note';
+    const noteButton = document.createElement('button');
+    noteButton.className = 'note-button'; 
+    noteButton.innerHTML = '&times;';
+    noteButton.setAttribute('onclick', `deleteNote(${i})`);
     const noteInfo = document.createElement('div');
     noteInfo.className = 'note-info';
     const textErea = document.createElement('textarea');
@@ -74,3 +114,19 @@ function renderNotes() {
   }
 }
 
+function returnList() {
+  return [
+    new Todo('jimbo', '18/06/2025', '17:25'),
+    new Todo("people are strange when your a stranger faces looks ugly when you're alone", '22/05/2025', '07:00'),
+    new Todo("There's a lady who's sure all that glitters is gold and she's buying a stairway to heaven", '18/06/2025', '17:25'),
+  ]
+}
+
+function deleteNote(index) {
+  for (let i = 0; i < todoListTotal.length; i++) {
+    if (i === index ) {
+      todoListTotal.splice(i, 1);
+      renderNotes1();
+    }
+  }
+}
