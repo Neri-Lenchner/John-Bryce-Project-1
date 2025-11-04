13f1fbdf3a88df8ed7c3b8141a93ea0c37e4a34b
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2025-11-04 09:36:53 +0200

    moved all the old for-loops functions that I had commented out, to the bottom of the js page

--- project-1/project-1.js
+++ project-1/project-1.js
@@ -20,16 +20,7 @@ renderNotes(false);
 /////////////////
 
 console.log('forEach-branch');
-/*
-function createTaskList() {
-  if (taskListTotalData.length) {
-    for (let i = 0; i < taskListTotalData.length; i++) {
-      const task = taskListTotalData[i];
-      taskListTotal.push(new Task(task.taskData, task.taskDate, task.taskTime));
-    }
-  }
-}
-*/
+
 function createTaskList() {
   if (taskListTotalData.length) {
     taskListTotalData.forEach((task) => {
@@ -64,6 +55,75 @@ function addTask(event) {
   renderNotes(true);
   console.log(taskListTotal);
 }
+
+function resetForm() {
+  const formTextErea = document.querySelector('#large-text-erea-element');
+  const inputs = document.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    input.value = '';
+  });
+  formTextErea.value = '';
+}
+
+function renderNotes(bulIsTrue) {
+  const notesMonitorElement = document.querySelector('#notes-monitor-element');
+  notesMonitorElement.innerHTML = '';
+  let lastNoteElement = null;
+
+  taskListTotal.forEach((task, i) => {
+
+    const note = document.createElement('div');
+    note.className = 'note';
+    const noteButton = document.createElement('button');
+    noteButton.className = 'note-button'; 
+    noteButton.innerHTML = '&times;';
+    noteButton.setAttribute('onclick', `deleteNoteByIndex(${i})`);
+    const noteInfo = document.createElement('div');
+    noteInfo.className = 'note-info';
+    const text = document.createElement('div');
+    text.innerText = task.taskData;
+    text.className = 'text';
+    const dateTimeContainer = document.createElement('div');
+    dateTimeContainer.className = 'date-time-container';
+    const date = document.createElement('div');
+    date.textContent = task.taskDate.split('-').reverse().join('/');
+    date.className = 'date';
+    const time = document.createElement('div');
+    time.className = 'time';
+    time.textContent = task.taskTime;
+    dateTimeContainer.append(date, time);
+    noteInfo.append(text, dateTimeContainer);
+    note.append(noteButton,noteInfo);
+    notesMonitorElement.appendChild(note);
+    lastNoteElement = note;
+  });
+  if (bulIsTrue) {
+    if (lastNoteElement) {
+      lastNoteElement.classList.add('last-note');
+    }
+  }
+}
+
+function deleteNote(index) {
+  taskListTotal.forEach((task, i) => {
+    if (i === index ) {
+      taskListTotal.splice(i, 1);
+      saveData();
+      if (taskListTotal.length === 0) {
+        localStorage.removeItem('taskList');
+      }
+      renderNotes(false);
+    }
+  });
+}
+
+function deleteNoteByIndex(index) {
+  deleteNote(index);
+}
+
+// Old for-loops functions //
+
 /*
 function resetForm() {
   const formTextErea = document.querySelector('#large-text-erea-element');
@@ -74,15 +134,17 @@ function resetForm() {
   formTextErea.value = '';
 }
 */
-function resetForm() {
-  const formTextErea = document.querySelector('#large-text-erea-element');
-  const inputs = document.querySelectorAll('input');
 
-  inputs.forEach((input) => {
-    input.value = '';
-  });
-  formTextErea.value = '';
+/*
+function createTaskList() {
+  if (taskListTotalData.length) {
+    for (let i = 0; i < taskListTotalData.length; i++) {
+      const task = taskListTotalData[i];
+      taskListTotal.push(new Task(task.taskData, task.taskDate, task.taskTime));
+    }
+  }
 }
+*/
 
 /*
 function renderNotes(bulIsTrue) {
@@ -128,44 +190,6 @@ function renderNotes(bulIsTrue) {
 }
 */
 
-function renderNotes(bulIsTrue) {
-  const notesMonitorElement = document.querySelector('#notes-monitor-element');
-  notesMonitorElement.innerHTML = '';
-  let lastNoteElement = null;
-
-  taskListTotal.forEach((task, i) => {
-
-    const note = document.createElement('div');
-    note.className = 'note';
-    const noteButton = document.createElement('button');
-    noteButton.className = 'note-button'; 
-    noteButton.innerHTML = '&times;';
-    noteButton.setAttribute('onclick', `deleteNoteByIndex(${i})`);
-    const noteInfo = document.createElement('div');
-    noteInfo.className = 'note-info';
-    const text = document.createElement('div');
-    text.innerText = task.taskData;
-    text.className = 'text';
-    const dateTimeContainer = document.createElement('div');
-    dateTimeContainer.className = 'date-time-container';
-    const date = document.createElement('div');
-    date.textContent = task.taskDate.split('-').reverse().join('/');
-    date.className = 'date';
-    const time = document.createElement('div');
-    time.className = 'time';
-    time.textContent = task.taskTime;
-    dateTimeContainer.append(date, time);
-    noteInfo.append(text, dateTimeContainer);
-    note.append(noteButton,noteInfo);
-    notesMonitorElement.appendChild(note);
-    lastNoteElement = note;
-  });
-  if (bulIsTrue) {
-    if (lastNoteElement) {
-      lastNoteElement.classList.add('last-note');
-    }
-  }
-}
 /*
 function deleteNote(index) {
   for (let i = 0; i < taskListTotal.length; i++) {
@@ -180,20 +204,3 @@ function deleteNote(index) {
   }
 }
 */
-
-function deleteNote(index) {
-  taskListTotal.forEach((task, i) => {
-    if (i === index ) {
-      taskListTotal.splice(i, 1);
-      saveData();
-      if (taskListTotal.length === 0) {
-        localStorage.removeItem('taskList');
-      }
-      renderNotes(false);
-    }
-  });
-}
-
-function deleteNoteByIndex(index) {
-  deleteNote(index);
-}
